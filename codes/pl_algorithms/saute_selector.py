9aaa8570e77100faa32c46a4bd0cdf80841c9333
PiotrDS <01180852@pw.edu.pl>
2025-10-13 14:03:38 +0200

    create project structure

---
+++ codes/pl_algorithms/saute_selector.py
@@ -0,0 +1,11 @@
+import numpy
+
+class saute_selector:
+
+    def __init__(self, X, y_pl) -> None:
+        self.X = X
+        self.y_pl = y_pl
+
+    def select(self, vars_no) -> numpy.array:
+        pass
+        
